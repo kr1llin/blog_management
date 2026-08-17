f32f0d4461268c0b5231ce67b6d788039104bf3d
Kirill Knyazkov <k.knyazkov@g.nsu.ru>
2026-08-17 18:07:29 +0700

    swagger

--- src/main/java/src/configs/SecurityConfig.java
+++ src/main/java/src/configs/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
                         .anyRequest().authenticated()
                 )
